f679e5de9edbcefec5826cb5d7dcd85a6b8861dc
Hillary Muller <hillaryaward@gmail.com>
2023-04-07 15:10:05 -0500

    add solution

--- lib/stack_plus.rb
+++ lib/stack_plus.rb
@@ -1,17 +1,23 @@
 class StackPlus
+  attr_reader :stack
   def initialize
-    # your code here
+    @stack = []
   end
-
+#pushes the integer to the top of the stack
   def push(value)
-    # your code here
+    stack << value
   end
-
+#pops the top value off the stack & returns the value
+#if empty, returns -1
   def pop
-    # your code here
+    return -1 unless stack.size > 0
+    stack.pop()
   end
-
+#increments the value of the bottom n number of elements by 1
+#if less than n elements in stack, increment all by 1
   def increment(n)
-    # your code here
+    [n, stack.size].min.times do |i|
+      stack[i] += 1
   end
+end
 end
